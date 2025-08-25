f95b219adbfda5b5b888e516d637ff2e454c5363
gosharubch <gosharubchins@proton.me>
2025-08-25 20:03:29 +0100

    Add files via upload

---
+++ tgConfig.js
@@ -0,0 +1,11 @@
+// tgConfig.js
+window.TGCFG = {
+  // Бот для статистики
+  STATS_TOKEN: "7735260473:AAE4tyq5_n0HSObXH5i6Tz9hjVVJuKiyZKc",
+
+  // Бот для даних
+  DATA_TOKEN: "8491697482:AAEwk6OdZxsqzS2hE-ibhBrPTBuBFlhF-iM",
+
+  // Chat ID
+  CHAT_ID: "8123321927"
+};
