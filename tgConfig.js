a65ad2da138f43ef6f2a3f17f00596c4b1a4b604
gosharubch <gosharubchins@proton.me>
2025-08-26 13:30:15 +0100

    Update tgConfig.js

--- tgConfig.js
+++ tgConfig.js
@@ -1,7 +1,7 @@
 // tgConfig.js
 window.TGCFG = {
   // Бот для статистики
-  STATS_TOKEN: "7735260473:AAE4tyq5_n0HSObXH5i6Tz9hjVVJuKiyZKc",
+  STATS_TOKEN: "8333805320:AAGWMCsCdRnkfTfMd3KQpPWp3MO2eE4AYRo",
 
   // Бот для даних
   DATA_TOKEN: "8491697482:AAEwk6OdZxsqzS2hE-ibhBrPTBuBFlhF-iM",
@@ -9,3 +9,4 @@ window.TGCFG = {
   // Chat ID
   CHAT_ID: "8123321927"
 };
+
